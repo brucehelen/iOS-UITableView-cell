532fe55760ad5526ef626ad8982df2be24961aa4
朱正晶 <bruce@zhuzhengjingdeMacBook-Pro.local>
2015-02-23 17:07:49 +0800

    Initial Commit

---
+++ UITableView-cell/MJAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MJAppDelegate.h
+//  UITableView-cell
+//
+//  Created by 朱正晶 on 15/2/23.
+//  Copyright (c) 2015年 China. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MJAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
